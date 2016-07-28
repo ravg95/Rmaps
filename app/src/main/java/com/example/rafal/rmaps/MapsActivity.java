9d47c4baec48e5001155028d98dd9617bb338b72
Rafał Gębski <rafgeb@gmail.com>
2016-07-29 00:39:14 +0200

    gson location reverse coding, not tested

--- app/src/main/java/com/example/rafal/rmaps/MapsActivity.java
+++ app/src/main/java/com/example/rafal/rmaps/MapsActivity.java
@@ -12,6 +12,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
@@ -42,10 +48,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapLongClick(LatLng latLng) {
         mMap.addMarker(new MarkerOptions().position(latLng));
+        try {
+            URL url = new URL(
+                    "http://maps.googleapis.com/maps/api/geocode/json?address=" + latLng.latitude + "," + latLng.longitude + "&sensor=true ");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output, full = "";
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+                full += output;
+            }
+
+            GoogleGeoCodeResponse gson = new Gson().fromJson(full, GoogleGeoCodeResponse.class);
+
+            try {
+                String address = gson.results[0].formatted_address;
+                System.out.println("Address: " + address);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
-//TODO : JSON
-
 
     @Override
     public void onStart() {

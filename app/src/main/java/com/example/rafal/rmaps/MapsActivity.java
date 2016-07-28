8e0e3ce7a538e6682ec780fd28195df1d50c082f
Rafał Gębski <rafgeb@gmail.com>
2016-07-29 01:46:46 +0200

    reverse geocodig working, result displayed in log

--- app/src/main/java/com/example/rafal/rmaps/MapsActivity.java
+++ app/src/main/java/com/example/rafal/rmaps/MapsActivity.java
@@ -1,10 +1,10 @@
 package com.example.rafal.rmaps;
 
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,8 +19,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
 
     private GoogleApiClient client;
@@ -48,43 +49,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapLongClick(LatLng latLng) {
         mMap.addMarker(new MarkerOptions().position(latLng));
-        try {
-            URL url = new URL(
-                    "http://maps.googleapis.com/maps/api/geocode/json?address=" + latLng.latitude + "," + latLng.longitude + "&sensor=true ");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output, full = "";
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-                full += output;
-            }
-
-            GoogleGeoCodeResponse gson = new Gson().fromJson(full, GoogleGeoCodeResponse.class);
-
-            try {
-                String address = gson.results[0].formatted_address;
-                System.out.println("Address: " + address);
-            } catch (NullPointerException e) {
-                e.printStackTrace();
-            }
-            conn.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        new GetGeoData().execute(latLng);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
+/*
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
@@ -96,15 +67,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // Otherwise, set the URL to null.
                 Uri.parse(null),
                 // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("null")
+                Uri.parse("android-app://com.example.rafal.rmaps/http/host/path")
         );
-        AppIndex.AppIndexApi.start(client, viewAction);
+        AppIndex.AppIndexApi.start(client, viewAction);*/
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
+    /*
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         Action viewAction = Action.newAction(
@@ -113,11 +84,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // TODO: If you have web page content that matches this app activity's content,
                 // make sure this auto-generated web page URL is correct.
                 // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
+                Uri.parse(null),
                 // TODO: Make sure this auto-generated app deep link URI is correct.
                 Uri.parse("android-app://com.example.rafal.rmaps/http/host/path")
         );
         AppIndex.AppIndexApi.end(client, viewAction);
+        */
         client.disconnect();
+    }
+
+    private class GetGeoData extends AsyncTask<LatLng, Void, String> {
+        @Override
+        protected String doInBackground(LatLng... latLng) {
+            String result = "";
+            try {
+                URL url = new URL(
+                        "http://maps.googleapis.com/maps/api/geocode/json?address=" + latLng[0].latitude + "," + latLng[0].longitude + "&sensor=true ");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Accept", "application/json");
+
+                if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+                }
+                BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+                String output, full = "";
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                    full += output;
+                }
+
+                GoogleGeoCodeResponse gson = new Gson().fromJson(full, GoogleGeoCodeResponse.class);
+
+                try {
+                    result = gson.results[0].formatted_address;
+
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+                conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(TAG, "found address:: " + result);
+        }
     }
 }

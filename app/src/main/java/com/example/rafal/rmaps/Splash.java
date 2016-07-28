869b2539f5e29291a0086ba01182b737f48cb289
Rafał Gębski <rafgeb@gmail.com>
2016-07-28 18:00:39 +0200

    Splash working

--- app/src/main/java/com/example/rafal/rmaps/Splash.java
+++ app/src/main/java/com/example/rafal/rmaps/Splash.java
@@ -1,13 +1,36 @@
 package com.example.rafal.rmaps;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
 
+    /**
+     * Duration of wait
+     **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_splash);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this, MapsActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
